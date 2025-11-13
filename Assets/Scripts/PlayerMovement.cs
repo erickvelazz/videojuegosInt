2772f357cf2166f65ad60d440a56955bf259caf5
JulianRR1 <rodriguezrojasjosejulian@gmail.com>
2025-11-13 03:10:43 -0600

    [ADD] Se areglo el salto, se crearon prefabs y se alargo el nivel

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,15 +5,15 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5.0f;
-    public float gravity = -25f; // ¡Asegúrate de que este valor sea alto!
+    public float gravity = -25f;
     public float rotationSpeed = 10f;
-    public float jumpHeight = 1.8f; // ¡Asegúrate de que este valor sea más bajo!
+    public float jumpHeight = 1.8f;
     public Animator animator;
 
     private CharacterController controller;
     private Vector3 velocity; 
     private Vector2 moveInput;
-    private bool isGrounded; // ¡NUEVO! Para guardar el estado
+    private bool isGrounded; // Para guardar el estado
 
     void Start()
     {
@@ -24,16 +24,13 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    // Esta función lee A/D
     public void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
     }
 
-    // Esta función lee el botón de Salto
     public void OnJump(InputValue value)
     {
-        // Solo saltamos si el botón fue presionado Y estamos en el suelo
         if (value.isPressed && isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
@@ -48,8 +45,9 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         // --- 1. CHEQUEO DE SUELO ---
-        // Hacemos esto primero y guardamos el estado
         isGrounded = controller.isGrounded;
+
+
 
         if (isGrounded && velocity.y < 0)
         {
@@ -68,13 +66,10 @@ public class PlayerMovement : MonoBehaviour
         }
         
         // --- 4. GRAVEDAD Y SALTO ---
-        // Aplicamos gravedad (o la fuerza de salto)
         velocity.y += gravity * Time.deltaTime; 
         controller.Move(velocity * Time.deltaTime); 
 
         // --- 5. ACTUALIZAR ANIMATOR ---
-        // ¡LÍNEAS MÁS IMPORTANTES!
-        // Le decimos al Animator CADA FOTOGRAMA si estamos en el suelo o no.
         if (animator != null)
         {
             animator.SetBool("isGrounded", isGrounded);
