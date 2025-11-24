b94a30b4d715a06c750382843527d8bc4f0ac5e3
Misael Cuaadra <20223tn090@utez.edu.mx>
2025-11-24 16:57:11 -0600

    [ADD]Unity fix movement

--- Assets/Scripts/PlayerMovementMisael.cs
+++ Assets/Scripts/PlayerMovementMisael.cs
@@ -3,55 +3,83 @@ using UnityEngine;
 public class PlayerMovementMisael : MonoBehaviour
 {
     [Header("Movement")]
-    public float moveSpeed = 7f;
+    public float moveSpeed = 6f;
+    private float horizontalInput;
 
-    [Header("Jumping")]
-    public float jumpForce = 12f;
+    [Header("Jump Settings")]
+    public float jumpForce = 12f;        // Primer salto
+    public float doubleJumpForce = 9f;   // Segundo salto más bajito
     public int maxJumps = 2;
-    private int jumpCount;
+
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
 
     private Rigidbody rb;
+    private int jumpCount;
     private bool isGrounded;
+
+    [Header("Better Jump Feel")]
+    public float fallMultiplier = 2.2f;
+    public float lowJumpMultiplier = 2.5f;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        jumpCount = maxJumps;
     }
 
     void Update()
     {
-        Move();
-        Jump();
-    }
+        if (horizontalInput > 0.1f)
+    transform.localScale = new Vector3(1, 1, 1);
+else if (horizontalInput < -0.1f)
+    transform.localScale = new Vector3(-1, 1, 1);
 
-    void Move()
-    {
-        float x = Input.GetAxisRaw("Horizontal");
-        Vector3 move = new Vector3(x * moveSpeed, rb.linearVelocity.y, 0);
-        rb.linearVelocity = move;
-    }
+    
+        horizontalInput = Input.GetAxisRaw("Horizontal");
 
-    void Jump()
-    {
-        if (isGrounded) jumpCount = maxJumps;
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundLayer);
 
-        if (Input.GetKeyDown(KeyCode.Space) && jumpCount > 0)
+        // Resetear los saltos cuando toca el piso
+        if (isGrounded)
+            jumpCount = 0;
+
+        // Saltar
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0, 0);
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            jumpCount--;
+            if (jumpCount == 0)       // Primer salto
+                Jump(jumpForce);
+            else if (jumpCount == 1)  // Doble salto
+                Jump(doubleJumpForce);
         }
+
+        BetterJumpPhysics();
     }
 
-    private void OnCollisionEnter(Collision collision)
+    void FixedUpdate()
     {
-        if (collision.contacts[0].normal.y > 0.5f)
-            isGrounded = true;
+        Vector3 movement = new Vector3(horizontalInput * moveSpeed, rb.linearVelocity.y, 0f);
+        rb.linearVelocity = movement;
     }
 
-    private void OnCollisionExit(Collision collision)
+    void Jump(float force)
     {
-        isGrounded = false;
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, 0f);
+        rb.AddForce(Vector3.up * force, ForceMode.Impulse);
+        jumpCount++;
+    }
+
+    void BetterJumpPhysics()
+    {
+        if (rb.linearVelocity.y < 0)
+        {
+            // Caída más pesada
+            rb.linearVelocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.linearVelocity.y > 0 && !Input.GetKey(KeyCode.Space))
+        {
+            // Si sueltas el espacio, sube menos (salto corto)
+            rb.linearVelocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
     }
 }
