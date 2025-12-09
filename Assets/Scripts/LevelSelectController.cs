323effec4af435a87b0151f70e5dcbf72c9a57a0
AldairVargas <aldair.vargas@entrypay.net>
2025-12-09 13:03:33 -0600

    Lvl layout

--- Assets/Scripts/LevelSelectController.cs
+++ Assets/Scripts/LevelSelectController.cs
@@ -5,13 +5,14 @@ using UnityEngine.UI;
 public class LevelSelectController : MonoBehaviour
 {
     public string nivel1 = "Level1Scene"; 
+    public string nivel2 = "Level2Scene";
     
     public Button btnNivel2;
     public Button btnNivel3;
 
     void Start()
     {
-        if(btnNivel2 != null) btnNivel2.interactable = false;
+        if(btnNivel2 != null) btnNivel2.interactable = true;
         if(btnNivel3 != null) btnNivel3.interactable = false;
     }
 
@@ -19,6 +20,11 @@ public class LevelSelectController : MonoBehaviour
     {
         Debug.Log("¡Botón presionado! Intentando cargar: " + nivel1); 
         SceneManager.LoadScene(nivel1);
+    }
+    
+    public void CargarNivel2()
+    {
+        SceneManager.LoadScene(nivel2);
     }
     
     public void VolverAlInicio()
